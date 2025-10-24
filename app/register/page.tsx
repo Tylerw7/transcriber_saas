912c8552d6740eab7e759d66c97854566ec4606b
Tylerw7 <t4wettach@gmail.com>
2025-10-24 17:43:14 -0400

    edited register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,17 +4,33 @@ import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 
 export default function RegisterPage() {
+
+
+
+
+    
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <Card className="w-full max-w-md shadow-xl">
         <CardHeader className="text-center">
-          <CardTitle className="text-2xl font-bold text-gray-900">Sign in to your account</CardTitle>
+          <CardTitle className="text-2xl font-bold text-gray-900">Create your account</CardTitle>
           <CardDescription className="text-gray-500 mt-2">
-            Enter your credentials below to access your dashboard
+            Enter your Name, Email and Password to create your account.
           </CardDescription>
         </CardHeader>
         <CardContent className="mt-4">
           <form className="space-y-4">
+          <div>
+              <Label htmlFor="email" className="text-gray-700 font-medium">
+                Name
+              </Label>
+              <Input
+                type="name"
+                id="name"
+                placeholder="name"
+                className="mt-1"
+              />
+            </div>
             <div>
               <Label htmlFor="email" className="text-gray-700 font-medium">
                 Email address
@@ -38,13 +54,13 @@ export default function RegisterPage() {
               />
             </div>
             <Button type="submit" size="lg" className="w-full bg-orange-500 hover:bg-orange-600 text-white rounded-full mt-2">
-              Sign In
+              Register
             </Button>
           </form>
           <div className="mt-6 text-center text-gray-500">
-            Don't have an account?{" "}
-            <a href="/register" className="text-orange-500 hover:underline">
-              Sign up
+            Already have an account?{" "}
+            <a href="/login" className="text-orange-500 hover:underline">
+              Login
             </a>
           </div>
         </CardContent>
